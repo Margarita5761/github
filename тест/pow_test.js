14cb24ea10283a7ff0f926f63aac26c5d81580ab
Margarita5761 <114502997+Margarita5761@users.noreply.github.com>
2023-03-15 15:34:39 +0700

    Create pow_test.js

---
+++ тест/pow_test.js
@@ -0,0 +1,8 @@
+import {pow} from '../main.js';
+import assert from 'node:assert/strict';
+
+assert(pow(8, 2) === 64)
+assert(pow(8, 1) === 8)
+assert(pow(8, 0) === 1)
+assert(pow(2, 2) === 3)
+assert(pow(3, 4) === 81)
